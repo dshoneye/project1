b7fc12df21eaffbaf84123e5f4832607ebdb58d9
Shoneye <David.Shoneye@us.sogeti.com>
2020-01-29 14:33:34 -0500

    this is a commit message

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state= {
+      employees: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3005/getSogetiEmployees')
+    .then(employees => employees.json())
+    .then(employees => {
+      console.log(employees)
+      this.setState({
+        employees: employees
+      })
+    })
+    .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div className="App container">
+        <h1>NBA Stars Directory</h1>
+        <div className="row">
+
+        { this.state.employees.map((employee, index) => {
+          return (
+            <div className="media col-12 mb-4 p-3 directory-entry">
+              <img src={employee.image} class="mr-3 directory-image" alt="..." />
+              <div className="media-body">
+                <h5 className="mt-0">{employee.name}</h5>
+                {employee.location}<div>{employee.position}</div>
+            </div>
+          </div>
+          )
+        })}
+      </div>
+      </div>
+    );
+  }
 }
 
 export default App;
